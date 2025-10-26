3cabe23e16be49ea769427d23ab385fd3746fdb2
Wang Chenye <213243019@seu.edu.cn>
2025-10-26 21:18:41 +0800

    test0

--- user/src/pid.c
+++ user/src/pid.c
@@ -1,7 +1,7 @@
 #include "pid.h"
 
 // 电机PID初始化参数
-float speed_KP = 50, speed_KI = 0,speed_KD = 0.0, speed_IMAX = 5000.0, speed_OUTMAX = 5000.0;
+float speed_KP = 150, speed_KI = 5,speed_KD = 0.0, speed_IMAX = 2000.0, speed_OUTMAX = 800.0;
 float speed_target = 30.0;
 float speed_real = 0.0;
 float speed_pwm = 0.0;
